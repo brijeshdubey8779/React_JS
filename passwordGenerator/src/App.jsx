ac6d9d582c5c60b502ece719272528bc50eb1c0e
brijeshdubey8779 <brijeshdubey8779@gmail.com>
2023-09-05 17:35:46 +0530

    ading project nothing coded yet adding with node folder

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
         />
         <button
           onClick={copyPasswordToClipboard}
-          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          className="outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0 hover:bg-blue-900 hover:text-cyan-200 "
         >
           copy
         </button>
